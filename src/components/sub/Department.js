a51d6c604ea450fca1b520b626e7942bbb9fe407
tubi55 <tubi55@nate.com>
2022-12-14 19:33:57 +0900

    멤버소개 페이지

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,7 +1,34 @@
 import Layout from '../common/Layout';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function Department() {
-	return <Layout name={'Department'}></Layout>;
+	const path = process.env.PUBLIC_URL;
+	const [Member, setMember] = useState([]);
+
+	useEffect(() => {
+		axios.get(`${path}/DB/members.json`).then((json) => {
+			setMember(json.data.members);
+		});
+	}, []);
+
+	return (
+		<Layout name={'Department'}>
+			{Member.map((data, idx) => {
+				return (
+					<article key={idx}>
+						<div className='inner'>
+							<div className='pic'>
+								<img src={`${path}/img/${data.pic}`} alt={data.name} />
+							</div>
+							<h3>{data.name}</h3>
+							<p>{data.position}</p>
+						</div>
+					</article>
+				);
+			})}
+		</Layout>
+	);
 }
 
 export default Department;
