92b389e536e07f599dc36f0b6fc5f23fa02ef765
tubi55 <tubi55@nate.com>
2022-12-15 15:48:47 +0900

    지동정보를 배열로 묶고 지점 버튼 생성 및 이벤트 연결

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -3,28 +3,48 @@ import { useRef, useEffect, useState } from 'react';
 
 function Location() {
 	const { kakao } = window;
+	const info = [
+		{
+			title: '넥슨 본사',
+			latlng: new kakao.maps.LatLng(37.40211707077346, 127.10344953763003),
+			imgUrl: `${process.env.PUBLIC_URL}/img/marker1.png`,
+			imgSize: new kakao.maps.Size(232, 99),
+			imgPos: { offset: new kakao.maps.Point(116, 99) },
+		},
+		{
+			title: '올림픽 공원',
+			latlng: new kakao.maps.LatLng(37.5206868, 127.1214941),
+			imgUrl: `${process.env.PUBLIC_URL}/img/marker2.png`,
+			imgSize: new kakao.maps.Size(232, 99),
+			imgPos: { offset: new kakao.maps.Point(116, 99) },
+		},
+		{
+			title: '서울 시청',
+			latlng: new kakao.maps.LatLng(37.5662952, 126.9779451),
+			imgUrl: `${process.env.PUBLIC_URL}/img/marker3.png`,
+			imgSize: new kakao.maps.Size(232, 99),
+			imgPos: { offset: new kakao.maps.Point(116, 99) },
+		},
+	];
+
 	const container = useRef(null);
 	const [Location, setLocation] = useState(null);
 	const [Traffic, setTraffic] = useState(false);
+	const [Info] = useState(info);
+	const [Index, setIndex] = useState(0);
 
-	//지도 기본 위치및 확대레벨 정보
 	const option = {
-		center: new kakao.maps.LatLng(37.40211707077346, 127.10344953763003),
+		center: Info[Index].latlng,
 		level: 3,
 	};
 
-	//마커의 위치 인스턴스
-	const markerPosition = new kakao.maps.LatLng(37.40211707077346, 127.10344953763003);
-
-	//마커 이미지 정보
-	const imageSrc = `${process.env.PUBLIC_URL}/img/marker1.png`;
-	const imageSize = new kakao.maps.Size(232, 99);
-	const imageOption = { offset: new kakao.maps.Point(116, 99) };
+	const markerPosition = Info[Index].latlng;
+	const imageSrc = Info[Index].imgUrl;
+	const imageSize = Info[Index].imgSize;
+	const imageOption = Info[Index].imgPos;
 
 	//마커 이미지 인스턴스 생성
 	const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
-
-	//마커 인스턴스 생성
 	const marker = new kakao.maps.Marker({
 		position: markerPosition,
 		image: markerImage,
@@ -34,7 +54,7 @@ function Location() {
 		const map_instance = new kakao.maps.Map(container.current, option);
 		marker.setMap(map_instance);
 		setLocation(map_instance);
-	}, []);
+	}, [Index]);
 
 	//Traffic state값이 변경될 때마다 지도 정보를 On, off처리
 	useEffect(() => {
@@ -47,6 +67,25 @@ function Location() {
 		<Layout name={'Location'}>
 			<div id='map' ref={container}></div>
 			<button onClick={() => setTraffic(!Traffic)}>{Traffic ? 'Traffic ON' : 'Traffic OFF'}</button>
+
+			<ul className='branch'>
+				{Info.map((el, idx) => {
+					let isOn = '';
+					Index === idx && (isOn = 'on');
+					return (
+						<li
+							key={idx}
+							className={isOn}
+							onClick={() => {
+								setIndex(idx);
+								setTraffic(false);
+							}}
+						>
+							{el.title}
+						</li>
+					);
+				})}
+			</ul>
 		</Layout>
 	);
 }
