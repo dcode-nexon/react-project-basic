5b68c6bfcd2d46271dd4d2191afc5dbc6a839a5c
tubi55 <tubi55@nate.com>
2022-12-16 19:22:22 +0900

    비밀번호 인증

--- src/components/sub/Member.js
+++ src/components/sub/Member.js
@@ -1,7 +1,146 @@
 import Layout from '../common/Layout';
+import { useState, useEffect } from 'react';
 
 function Member() {
-	return <Layout name={'Member'}></Layout>;
+	const initVal = {
+		userid: '',
+		email: '',
+		pwd1: '',
+		pwd2: '',
+	};
+
+	const [Val, setVal] = useState(initVal);
+	const [Err, setErr] = useState({});
+
+	const check = (value) => {
+		const errs = {};
+		const eng = /[a-zA-Z]/;
+		const num = /[0-9]/;
+		const spc = /[\!\@\#\$\%\^\&\*\(\)\_\+]/;
+
+		if (value.userid.length < 5) {
+			errs.userid = '아이디를 5글자이상 입력하세요';
+		}
+		if (
+			value.pwd1.length < 5 ||
+			!eng.test(value.pwd1) ||
+			!num.test(value.pwd1) ||
+			!spc.test(value.pwd1)
+		) {
+			errs.pwd1 = '비밀번호는 다섯글자 이상, 영문, 숫자, 특수문자를 모두 포함하세요';
+		}
+		if (value.pwd1 !== value.pwd2) {
+			errs.pwd2 = '두개의 비밀번호를 동일하게 입력하세요.';
+		}
+		if (value.email.length < 8 || !/@/.test(Val.email)) {
+			errs.email = '이메일은 8글자 이상 입력, @를 포함하세요.';
+		}
+		return errs;
+	};
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setVal({ ...Val, [name]: value });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setErr(check(Val));
+	};
+
+	useEffect(() => {
+		console.log(Err);
+	}, [Err]);
+
+	return (
+		<Layout name={'Member'}>
+			<form action='' onSubmit={handleSubmit}>
+				<fieldset>
+					<legend className='h'>회원가입 폼 양식</legend>
+					<table border='1'>
+						<caption className='h'>회원가입 정보입력</caption>
+						<tbody>
+							{/* userid */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='userid'>USER ID</label>
+								</th>
+								<td>
+									<input
+										type='text'
+										placeholder='아이디를 입력하세요'
+										name='userid'
+										id='userid'
+										value={Val.userid}
+										onChange={handleChange}
+									/>
+									<span className='err'>{Err.userid}</span>
+								</td>
+							</tr>
+
+							{/* password */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='pwd1'>PASSWORD</label>
+								</th>
+								<td>
+									<input
+										type='password'
+										name='pwd1'
+										id='pwd1'
+										placeholder='비밀번호를 입력하세요'
+										onChange={handleChange}
+									/>
+									<span className='err'>{Err.pwd1}</span>
+								</td>
+							</tr>
+							<tr>
+								<th scope='row'>
+									<label htmlFor='pwd2'>RE-PASSWORD</label>
+								</th>
+								<td>
+									<input
+										type='password'
+										name='pwd2'
+										id='pwd2'
+										placeholder='비밀번호를 재입력하세요'
+										onChange={handleChange}
+									/>
+									<span className='err'>{Err.pwd2}</span>
+								</td>
+							</tr>
+
+							{/* email */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='email'>E-MAIL</label>
+								</th>
+								<td>
+									<input
+										type='text'
+										id='email'
+										name='email'
+										placeholder='이메일 주소를 입력하세요.'
+										value={Val.email}
+										onChange={handleChange}
+									/>
+									<span className='err'>{Err.email}</span>
+								</td>
+							</tr>
+
+							{/* btn set */}
+							<tr>
+								<th colSpan='2'>
+									<input type='reset' value='reset' />
+									<input type='submit' value='submit' />
+								</th>
+							</tr>
+						</tbody>
+					</table>
+				</fieldset>
+			</form>
+		</Layout>
+	);
 }
 
 export default Member;
