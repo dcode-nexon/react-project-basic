44a611aa515b7e862d54e99fdd98f42de961baea
tubi55 <tubi55@nate.com>
2022-12-16 19:39:16 +0900

    checkbox 인증처리

--- src/components/sub/Member.js
+++ src/components/sub/Member.js
@@ -7,6 +7,8 @@ function Member() {
 		email: '',
 		pwd1: '',
 		pwd2: '',
+		gender: null,
+		interests: null,
 	};
 
 	const [Val, setVal] = useState(initVal);
@@ -35,12 +37,35 @@ function Member() {
 		if (value.email.length < 8 || !/@/.test(Val.email)) {
 			errs.email = '이메일은 8글자 이상 입력, @를 포함하세요.';
 		}
+		if (!Val.gender) {
+			errs.gender = '성별을 선택하세요.';
+		}
+		if (!Val.interests) {
+			errs.interests = '관심사를 하나이상 선택하세요.';
+		}
 		return errs;
 	};
 
 	const handleChange = (e) => {
 		const { name, value } = e.target;
 		setVal({ ...Val, [name]: value });
+	};
+
+	const handleRadio = (e) => {
+		const { name } = e.target;
+		const isChecked = e.target.checked;
+		setVal({ ...Val, [name]: isChecked });
+	};
+
+	const handleCheck = (e) => {
+		let isChecked = false;
+		const { name } = e.target;
+		const inputs = e.target.parentElement.querySelectorAll('input');
+
+		inputs.forEach((el) => {
+			if (el.checked) isChecked = true;
+		});
+		setVal({ ...Val, [name]: isChecked });
 	};
 
 	const handleSubmit = (e) => {
@@ -125,6 +150,37 @@ function Member() {
 										onChange={handleChange}
 									/>
 									<span className='err'>{Err.email}</span>
+								</td>
+							</tr>
+
+							{/* gender */}
+							<tr>
+								<th scope='row'>GENDER</th>
+								<td>
+									<label htmlFor='male'>Male</label>
+									<input type='radio' name='gender' id='mail' onChange={handleRadio} />
+
+									<label htmlFor='female'>FeMale</label>
+									<input type='radio' name='gender' id='female' onChange={handleRadio} />
+
+									<span className='err'>{Err.gender}</span>
+								</td>
+							</tr>
+
+							{/* interests */}
+							<tr>
+								<th scope='row'>INTERESTS</th>
+								<td>
+									<label htmlFor='sports'>SPORTS</label>
+									<input type='checkbox' name='interests' id='sports' onChange={handleCheck} />
+
+									<label htmlFor='music'>MUSIC</label>
+									<input type='checkbox' name='interests' id='music' onChange={handleCheck} />
+
+									<label htmlFor='game'>GAME</label>
+									<input type='checkbox' name='interests' id='game' onChange={handleCheck} />
+
+									<span className='err'>{Err.interests}</span>
 								</td>
 							</tr>
 
