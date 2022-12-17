253d4ae2153ff66149bd0302e0ec121ca2468861
tubi55 <tubi55@nate.com>
2022-12-17 19:07:41 +0900

    버튼 클릭시 자동 스크롤 이동

--- src/components/main/Btns.js
+++ src/components/main/Btns.js
@@ -1,8 +1,12 @@
+import { faSpeakerDeck } from '@fortawesome/free-brands-svg-icons';
 import { useRef, useEffect } from 'react';
+import Anime from '../../asset/Anime';
 
 function Btns() {
 	const pos = useRef([]);
 	const btnRef = useRef(null);
+	const num = 4;
+	const speed = 500;
 
 	const getPos = () => {
 		pos.current = [];
@@ -13,17 +17,22 @@ function Btns() {
 	const activation = () => {
 		const base = -window.innerHeight / 2;
 		const btns = btnRef.current.children;
+		const secs = btnRef.current.parentElement.querySelectorAll('.myScroll');
 		const scroll = window.scrollY;
 
 		pos.current.map((pos, idx) => {
 			if (scroll >= pos + base) {
 				for (const btn of btns) btn.classList.remove('on');
+				for (const sec of secs) sec.classList.remove('on');
 				btns[idx].classList.add('on');
+				secs[idx].classList.add('on');
 			}
 		});
 	};
 
 	useEffect(() => {
+		btnRef.current.children[0].classList.add('on');
+		btnRef.current.parentElement.querySelectorAll('.myScroll')[0].classList.add('on');
 		getPos();
 		window.addEventListener('resize', getPos);
 		window.addEventListener('scroll', activation);
@@ -35,10 +44,20 @@ function Btns() {
 
 	return (
 		<ul className='scroll_navi' ref={btnRef}>
-			<li className='on'></li>
-			<li></li>
-			<li></li>
-			<li></li>
+			{Array(num)
+				.fill()
+				.map((_, idx) => (
+					<li
+						key={idx}
+						onClick={() => {
+							new Anime(window, {
+								prop: 'scroll',
+								value: pos.current[idx],
+								duration: speed,
+							});
+						}}
+					></li>
+				))}
 		</ul>
 	);
 }
