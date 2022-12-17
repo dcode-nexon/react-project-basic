99c3e3f0c2247df0f1467f5ba44c6ee75ea523f0
tubi55 <tubi55@nate.com>
2022-12-17 18:51:40 +0900

    메인 컴포넌트에서 window이벤트 연결문을 제거하고 btns컴포넌트에서 resize, scroll이벤트 연결

--- src/components/main/Btns.js
+++ src/components/main/Btns.js
@@ -1,10 +1,44 @@
-function Btns({ setIndex }) {
+import { useRef, useEffect } from 'react';
+
+function Btns() {
+	const pos = useRef([]);
+	const btnRef = useRef(null);
+
+	const getPos = () => {
+		pos.current = [];
+		const secs = btnRef.current.parentElement.querySelectorAll('.myScroll');
+		for (const sec of secs) pos.current.push(sec.offsetTop);
+	};
+
+	const activation = () => {
+		const base = -window.innerHeight / 2;
+		const btns = btnRef.current.children;
+		const scroll = window.scrollY;
+
+		pos.current.map((pos, idx) => {
+			if (scroll >= pos + base) {
+				for (const btn of btns) btn.classList.remove('on');
+				btns[idx].classList.add('on');
+			}
+		});
+	};
+
+	useEffect(() => {
+		getPos();
+		window.addEventListener('resize', getPos);
+		window.addEventListener('scroll', activation);
+		return () => {
+			window.removeEventListener('resize', getPos);
+			window.removeEventListener('scroll', activation);
+		};
+	}, []);
+
 	return (
-		<ul className='scroll_navi'>
-			<li className='on' onClick={() => setIndex(0)}></li>
-			<li onClick={() => setIndex(1)}></li>
-			<li onClick={() => setIndex(2)}></li>
-			<li onClick={() => setIndex(3)}></li>
+		<ul className='scroll_navi' ref={btnRef}>
+			<li className='on'></li>
+			<li></li>
+			<li></li>
+			<li></li>
 		</ul>
 	);
 }
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -4,42 +4,16 @@ import News from './News';
 import Pics from './Pics';
 import Vids from './Vids';
 import Btns from './Btns';
-import { useEffect, useRef, useState } from 'react';
-import Anime from '../../asset/Anime';
 
 function Main() {
-	const main = useRef(null);
-	const pos = useRef([]);
-
-	const [Index, setIndex] = useState(0);
-
-	const getPos = () => {
-		const secs = main.current.querySelectorAll('.myScroll');
-		for (const sec of secs) pos.current.push(sec.offsetTop);
-	};
-
-	useEffect(() => {
-		getPos();
-		window.addEventListener('resize', getPos);
-		return () => window.removeEventListener('resize', getPos);
-	}, []);
-
-	useEffect(() => {
-		new Anime(window, {
-			prop: 'scroll',
-			value: pos.current[Index],
-			duration: 500,
-		});
-	}, [Index]);
-
 	return (
-		<main ref={main}>
+		<main>
 			<Header type={'main'} />
 			<Visual />
 			<News />
 			<Pics />
 			<Vids />
-			<Btns setIndex={setIndex} />
+			<Btns />
 		</main>
 	);
 }
