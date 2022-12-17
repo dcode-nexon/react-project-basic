199f2def12fb6a50374e3295c8c0ecfb2c740179
tubi55 <tubi55@nate.com>
2022-12-17 14:33:21 +0900

    게시글 수정

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -5,13 +5,20 @@ import axios from 'axios';
 function Community() {
 	const input = useRef(null);
 	const textarea = useRef(null);
+	const inputEdit = useRef(null);
+	const textareaEdit = useRef(null);
 	const [Posts, setPosts] = useState([]);
+	const [Allowed, setAllowed] = useState(true);
 
+	//input요소 초기화 함수
 	const resetForm = () => {
 		input.current.value = '';
 		textarea.current.value = '';
+		inputEdit.current.value = '';
+		textareaEdit.current.value = '';
 	};
 
+	//게시글 생성함수
 	const createPost = () => {
 		if (!input.current.value.trim() || !textarea.current.value.trim()) {
 			resetForm();
@@ -21,8 +28,52 @@ function Community() {
 		resetForm();
 	};
 
+	//게시글 삭제함수
 	const deletePost = (index) => {
-		setPosts(Posts.filter((post, idx) => idx !== index));
+		setPosts(Posts.filter((_, idx) => idx !== index));
+	};
+
+	//게시글 수정함수
+	const updatePost = (index) => {
+		if (!inputEdit.current.value.trim() || !textareaEdit.current.value.trim()) {
+			resetForm();
+			return alert('수정할 제목과 본문을 모두 입력하세요.');
+		}
+		setAllowed(true);
+
+		setPosts(
+			Posts.map((post, idx) => {
+				if (idx === index) {
+					post.title = inputEdit.current.value;
+					post.content = textareaEdit.current.value;
+					post.enableUpdate = false;
+				}
+				return post;
+			})
+		);
+	};
+
+	//게시글 수정모드 변경함수
+	const enableUpdate = (index) => {
+		if (!Allowed) return;
+		setAllowed(false);
+		setPosts(
+			Posts.map((post, idx) => {
+				if (idx === index) post.enableUpdate = true;
+				return post;
+			})
+		);
+	};
+
+	//게시글 출력모드 변경함수
+	const disableUpdate = (index) => {
+		setAllowed(true);
+		setPosts(
+			Posts.map((post, idx) => {
+				if (idx === index) post.enableUpdate = false;
+				return post;
+			})
+		);
 	};
 
 	useEffect(() => {
@@ -53,15 +104,39 @@ function Community() {
 				{Posts.map((post, idx) => {
 					return (
 						<article key={idx}>
-							<div className='txt'>
-								<h2>{post.title}</h2>
-								<p>{post.content}</p>
-							</div>
+							{post.enableUpdate ? (
+								//수정모드
+								<>
+									<div className='editTxt'>
+										<input type='text' defaultValue={post.title} ref={inputEdit} />
+										<br />
+										<textarea
+											cols='30'
+											rows='4'
+											defaultValue={post.content}
+											ref={textareaEdit}
+										></textarea>
+									</div>
 
-							<div className='btnSet'>
-								<button>EDIT</button>
-								<button onClick={() => deletePost(idx)}>DELETE</button>
-							</div>
+									<div className='btnSet'>
+										<button onClick={() => disableUpdate(idx)}>CANCEL</button>
+										<button onClick={() => updatePost(idx)}>UPDATE</button>
+									</div>
+								</>
+							) : (
+								//출력 모드
+								<>
+									<div className='txt'>
+										<h2>{post.title}</h2>
+										<p>{post.content}</p>
+									</div>
+
+									<div className='btnSet'>
+										<button onClick={() => enableUpdate(idx)}>EDIT</button>
+										<button onClick={() => deletePost(idx)}>DELETE</button>
+									</div>
+								</>
+							)}
 						</article>
 					);
 				})}
