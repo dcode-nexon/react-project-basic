b516dd9b90a3c9035c0f118d68e8a74f82441eac
tubi55 <tubi55@nate.com>
2022-12-17 21:21:09 +0900

    데이터 요청함수에 타입지정

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -1,40 +1,56 @@
 import Layout from '../common/Layout';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import Masonry from 'react-masonry-component';
 
 function Gallery() {
-	const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
-	const key = 'ae5dbef0587895ed38171fcda4afb648';
-	const method_interest = 'flickr.interestingness.getList';
-	const num = 20;
-	const url = `${baseURL}&method=${method_interest}&api_key=${key}&per_page=${num}`;
+	const masonryOptions = { transitionDuration: '0.5s' };
 
 	const [Items, setItems] = useState([]);
 
-	useEffect(async () => {
+	const getFlickr = async (opt) => {
+		const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
+		const key = 'ae5dbef0587895ed38171fcda4afb648';
+		const method_interest = 'flickr.interestingness.getList';
+		const method_search = 'flickr.photos.search';
+		const num = 40;
+		let url = '';
+
+		if (opt.type === 'interest')
+			url = `${baseURL}&method=${method_interest}&api_key=${key}&per_page=${num}`;
+		if (opt.type === 'search')
+			url = `${baseURL}&method=${method_search}&api_key=${key}&per_page=${num}&tags=${opt.tags}`;
+
 		const result = await axios.get(url);
 		console.log(result.data.photos.photo);
 		setItems(result.data.photos.photo);
+	};
+
+	useEffect(async () => {
+		//getFlickr({ type: 'search', tags: '하늘' });
+		getFlickr({ type: 'interest' });
 	}, []);
 
 	return (
 		<Layout name={'Gallery'}>
 			<div className='frame'>
-				{Items.map((item, idx) => {
-					return (
-						<article key={item.id}>
-							<div className='inner'>
-								<div className='pic'>
-									<img
-										src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`}
-										alt={item.title}
-									/>
+				<Masonry elementType={'div'} options={masonryOptions}>
+					{Items.map((item, idx) => {
+						return (
+							<article key={item.id}>
+								<div className='inner'>
+									<div className='pic'>
+										<img
+											src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`}
+											alt={item.title}
+										/>
+									</div>
+									<h2>{item.title}</h2>
 								</div>
-								<h2>{item.title}</h2>
-							</div>
-						</article>
-					);
-				})}
+							</article>
+						);
+					})}
+				</Masonry>
 			</div>
 		</Layout>
 	);
